9700ebbc570ec130cc61366c0de7d897ea0adefd
rafal1991 <rafal.radwanski@yahoo.pl>
2020-04-19 04:36:36 +0200

    added max health, styled text and fixed potion healing

--- Pokemon_master.py
+++ Pokemon_master.py
@@ -11,9 +11,9 @@ class Pokemon:
 
     def lose_health(self, health_loss):
 
-        print("{name} had {health} health before damage".format(name=self.name, health=self.current_health))
+        print("{name} had {health}/{max_health} health before damage".format(name=self.name, health=self.current_health, max_health=self.maximum_health))
         self.current_health = self.current_health - health_loss
-        print("{name} now has {health} health".format(name=self.name, health=self.current_health))
+        print("{name} now has {health}/{max_health} health \n".format(name=self.name, health=self.current_health, max_health=self.maximum_health))
 
     def regain_health(self, health_gain):
         after_potion = self.current_health + health_gain
@@ -21,7 +21,7 @@ class Pokemon:
             self.current_health = self.current_health + health_gain
         if after_potion > self.maximum_health:
             self.current_health = self.maximum_health
-        print("{name} now has {health} health".format(name=self.name, health=self.current_health))
+        print("{name} now has {health}/{max_health} health".format(name=self.name, health=self.current_health, max_health=self.maximum_health))
 
     def knock_out(self):
         if self.current_health <= 0:
@@ -76,22 +76,22 @@ class Trainer():
     def use_potion(self):
         # reduces num_of_potions by 1
         # heals active pokemon
-
-        potion_healing = self.regain_health(16)
-        self.active_pokemon.current_health = self.active_pokemon.potion_healing
+        print(self.trainer_name)
+        amount_of_healing = 20
+        potion_healing = self.active_pokemon.regain_health(amount_of_healing)
+        potion_healing
         self.num_of_potions += -1
-        print('Your pokemon has been healed\n')
+        print("{trainer_name}'s {pokemon} has been heald by {healing} \n".format(trainer_name=self.trainer_name, pokemon=self.active_pokemon.name, healing=amount_of_healing))
 
     def attack_trainer(self, trainer_to_attack):
         # give damage to another trainer's active pokemon
         pokemon_we_attack = trainer_to_attack.active_pokemon
         attack_damage = self.active_pokemon.attack(pokemon_we_attack)
-        #health_loss_opposite_pokemon = pokemon_we_attack.lose_health(attack_damage)
         pokemon_we_attack.lose_health(attack_damage)
-        print("{trainer_name}'s {pokemon} delt {attack_damage} damage \n".format(trainer_name=self.trainer_name, pokemon=self.active_pokemon.name, attack_damage=attack_damage))
 
 
     def switch_pokemon(self, pokemon_number):
+        print(self.trainer_name)
         self.active_pokemon = self.pokemons[pokemon_number - 1]
         print("{trainer} chose {name}\n".format(trainer=self.trainer_name, name=self.active_pokemon.name))
 
