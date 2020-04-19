81168feb4aa2b48e01a650df793c8e6128358c3c
rafal1991 <rafal.radwanski@yahoo.pl>
2020-04-19 03:44:50 +0200

    showing name of trainer each round. Potion not working

--- Pokemon_master.py
+++ Pokemon_master.py
@@ -6,8 +6,12 @@ class Pokemon:
         self.maximum_health = self.level * 5
         self.current_health = self.maximum_health
         self.is_knocked_out = is_knocked_out
+        self.trainer_name = None
+
 
     def lose_health(self, health_loss):
+
+        print("{name} had {health} health before damage".format(name=self.name, health=self.current_health))
         self.current_health = self.current_health - health_loss
         print("{name} now has {health} health".format(name=self.name, health=self.current_health))
 
@@ -54,17 +58,20 @@ class Pokemon:
                 attack_multiplier = 1
 
         attack_damage = self.level * attack_multiplier
+        print(self.trainer_name)
         print("{name} is dealing {damage} damage to the opposite pokemon".format(name=self.name, damage=attack_damage))
         return attack_damage
 
 
-class Trainer(Pokemon):
+class Trainer():
     def __init__(self, pokemons, trainer_name, num_of_potions, active_pokemon):
-        #super().__init__(name, level, type, current_health, is_knocked_out)
         self.pokemons = pokemons
         self.trainer_name = trainer_name
         self.num_of_potions = num_of_potions
         self.active_pokemon = self.pokemons[active_pokemon -1]
+        for pokemon in pokemons:
+            pokemon.trainer_name = self.trainer_name
+
 
     def use_potion(self):
         # reduces num_of_potions by 1
@@ -73,19 +80,20 @@ class Trainer(Pokemon):
         potion_healing = self.regain_health(16)
         self.active_pokemon.current_health = self.active_pokemon.potion_healing
         self.num_of_potions += -1
-        print('Your pokemon has been healed')
+        print('Your pokemon has been healed\n')
 
     def attack_trainer(self, trainer_to_attack):
         # give damage to another trainer's active pokemon
         pokemon_we_attack = trainer_to_attack.active_pokemon
-        attack_damage = self.attack(pokemon_we_attack)
-        self.attack(pokemon_we_attack.lose_health(attack_damage))
-        print("Your pokemon delt " + str(attack_damage) + " damage")
+        attack_damage = self.active_pokemon.attack(pokemon_we_attack)
+        #health_loss_opposite_pokemon = pokemon_we_attack.lose_health(attack_damage)
+        pokemon_we_attack.lose_health(attack_damage)
+        print("{trainer_name}'s {pokemon} delt {attack_damage} damage \n".format(trainer_name=self.trainer_name, pokemon=self.active_pokemon.name, attack_damage=attack_damage))
 
 
     def switch_pokemon(self, pokemon_number):
         self.active_pokemon = self.pokemons[pokemon_number - 1]
-        print("You chose {name}".format(name=self.active_pokemon.name))
+        print("{trainer} chose {name}\n".format(trainer=self.trainer_name, name=self.active_pokemon.name))
 
 
 
@@ -99,4 +107,13 @@ growlithe = Pokemon('growlithe', 7, 'fire', False)
 Ash_keczap = Trainer([charmander, bulbasaur, squirtle], "Ash", 3, 1)
 justin_timberlake = Trainer([celebi, lapras, growlithe], "Justin", 3, 1)
 
+# Possible moves:
+# attack my pokemon
+# switch active pokemon
+# use potion
+
 Ash_keczap.attack_trainer(justin_timberlake)
+justin_timberlake.switch_pokemon(3)
+Ash_keczap.attack_trainer((justin_timberlake))
+justin_timberlake.attack_trainer(Ash_keczap)
+Ash_keczap.use_potion()
